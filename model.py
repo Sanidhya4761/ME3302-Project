88d3eb15ff4fa615d814ed0aee550158322faa96
Sanidhya4761 <me22b189@smail.iitm.ac.in>
2025-05-02 21:49:47 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,8 +1,14 @@
 from tensorflow.keras.models import load_model
-
+import cv2
 # Load the pre-trained model
 model = load_model('model.h5')
-
+image =cv2.imread("testData/img_0242.jpg")
+image = cv2.resize(image, (256, 256))
+image = tf.expand_dims(image,axis=0)
+print(image.shape)
 #Replace with ur custom file
-model.predict('testfile.jpeg') 
-model.summary()
+k = (model.predict(image))
+if(k>=0.5):
+  print('No defect')
+else:
+  print("Defected")
